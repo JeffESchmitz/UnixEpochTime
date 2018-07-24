7602ecc01342bac96567f4bdcc8bf01960854221
vs2017-dev-1\ecolab <jeffeschmitz@gmail.com>
2018-07-24 15:29:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(":: ConvertFromEpochToDateTime - with double ::");
+            Console.WriteLine();
+
+            const double testEpoch1 = 1530787490958.74;
+            ConvertFromEpochToDateTime(testEpoch1);
+            Console.WriteLine();
+
+            const double testEpoch2 = 1530787490958.7;
+            ConvertFromEpochToDateTime(testEpoch2);
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+
+        private static void ConvertFromEpochToDateTime(double epochValue)
+        {
+            var unixStartDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var resultDateTime = unixStartDateTime.AddMilliseconds(epochValue).ToLocalTime();
+            Console.WriteLine(resultDateTime.ToString("O"));
+            Console.WriteLine($"Ticks: {resultDateTime.Ticks}");
+        }
+    }
+}
